e7251f2486de95664ca9f730ea812a1353cdd1e4
Zach Liibbe <zliibbe@gmail.com>
2023-04-25 20:45:32 -0600

    Update post function

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -18,6 +18,14 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
