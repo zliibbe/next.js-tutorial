c48d52155c12d21fe2b6acb4ea29c1a6efce9f82
Zach Liibbe <zliibbe@gmail.com>
2023-04-25 20:54:04 -0600

    Add head to post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -21,6 +22,9 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
